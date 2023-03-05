5c533fc4c9e43a7bac98b9ea85051dd63cd3b5c7
Luke Freedman <lfreedma@fandm.edu>
2023-03-05 17:34:58 -0500

    "pray"

--- app/src/main/java/edu/fandm/mobapp/worldy/Main.java
+++ app/src/main/java/edu/fandm/mobapp/worldy/Main.java
@@ -13,11 +13,25 @@ public class Main extends AppCompatActivity {
         if (word1.length() != word2.length()) {
         return null;
         }
-        result = find_matches();
-        return null;
+        if (word_dict != null && word_dict.length != 0) {
+
+        }
+        result = find_matches(word_dict, word1, word2, 0, word1.length());
+        return result;
     }
 
-    public String[] find_matches() {
+    public String[] find_matches(String[] list, String curr, String dest, int try_num, int try_max) {
+        if (try_num == try_max) {
+            return null;
+        }
+        for (String entry : list) {
+            int correct = 0;
+            for (int i = 0; i < curr.length(); i++) {
+                if (entry.substring(i,i) == curr.substring(i,i)) {
+                    correct++;
+                }
+            }
+        }
         return null;
     }
 
