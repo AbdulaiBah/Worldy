9ac7da9617bd766343d0494468b21276dc7606e1
Luke Freedman <lfreedma@fandm.edu>
2023-03-05 17:07:42 -0500

    "Made Main Activity ish"

--- app/src/main/java/edu/fandm/mobapp/worldy/Main.java
+++ app/src/main/java/edu/fandm/mobapp/worldy/Main.java
@@ -6,6 +6,21 @@ import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
+    public String[] word_dict = {"BEAR", "BORE", "DARE", "DOOR", "CHORE", "AIR", "CARE", "WINE", "BEER", "WIND", "FIND", "BIND"};
+
+    public String[] search_for_match(String word1, String word2, int x) {
+        String[] result;
+        if (word1.length() != word2.length()) {
+        return null;
+        }
+        result = find_matches();
+        return null;
+    }
+
+    public String[] find_matches() {
+        return null;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
