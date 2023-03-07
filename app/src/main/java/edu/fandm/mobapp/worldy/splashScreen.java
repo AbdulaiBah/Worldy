ed1ebb12b73e098df73bcf2f58551e51fdde36a2
Abdulai Razzaq Bah <abah@fandm.edu>
2023-03-07 13:09:29 -0500

    added splash_screen features

--- app/src/main/java/edu/fandm/mobapp/worldy/splashScreen.java
+++ app/src/main/java/edu/fandm/mobapp/worldy/splashScreen.java
@@ -1,14 +1,54 @@
 package edu.fandm.mobapp.worldy;
 
+import static android.app.ProgressDialog.show;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
 
+import edu.fandm.mobapp.worldy.databinding.ActivitySplashScreenBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
 public class splashScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.activity_splash_screen);
+
+        View view = findViewById(R.id.splashScreen);
+
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), ConfigurationScreen.class);
+                startActivity(i);
+            }
+        }, 1375);
+
     }
 }
