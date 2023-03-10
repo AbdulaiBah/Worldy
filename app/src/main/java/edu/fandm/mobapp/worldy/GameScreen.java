7917d4f0b46003eda51c1a031ff498c2c046faa4
Abdulai Razzaq Bah <abah@fandm.edu>
2023-03-10 01:02:48 -0500

    attempt to implement game screen, need path and images functions from game config file

--- app/src/main/java/edu/fandm/mobapp/worldy/GameScreen.java
+++ app/src/main/java/edu/fandm/mobapp/worldy/GameScreen.java
@@ -3,12 +3,49 @@ package edu.fandm.mobapp.worldy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GameScreen extends AppCompatActivity {
+
+    ArrayList<String> path;
+    String[] currentGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+
+        currentGuess = new String[path.size()];
+        String startWord = path.get(0);
+        String endWord = path.get(path.size() - 1);
+        currentGuess[0] = startWord;
+        currentGuess[path.size() - 1] = endWord;
+
+
+
     }
+
+    public void hintButton(View view) {
+
+    }
+
+
+
+    public boolean checkWord(String word) {
+        int i = Arrays.asList(currentGuess).indexOf(word);
+        return i == path.indexOf(word);
+    }
+
+
+    public void getImage() {
+        for (int i = 0; i < path.size(); i++) {
+            String word = path.get(i);
+
+    }
+
+
 }
