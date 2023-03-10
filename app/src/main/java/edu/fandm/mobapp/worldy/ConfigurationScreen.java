43d3ef3de6331def26263d29e3e1e349ab8f30a0
Luke Freedman <lfreedma@fandm.edu>
2023-03-09 21:56:34 -0500

    "The search algorithm now works as intended! It returns an ArrayList rather than a String[], but that's easily fixable/convertible"

--- app/src/main/java/edu/fandm/mobapp/worldy/ConfigurationScreen.java
+++ app/src/main/java/edu/fandm/mobapp/worldy/ConfigurationScreen.java
@@ -35,6 +35,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -45,7 +46,7 @@ public class ConfigurationScreen extends AppCompatActivity {
 
     public int mode;
     public String TAG = "Configuration Screen";
-    public String word_source = "bear";
+    public String word_source = "cats";
     public ArrayList<String> word_dict;
     public static String fileName;
 
@@ -56,6 +57,15 @@ public class ConfigurationScreen extends AppCompatActivity {
     public interface GetImageCallback {
         void onComplete(Bitmap bp);
     }
+
+    public ArrayList<String> populate() {
+        String[] dummy = {"wane", "vane", "cots", "cats", "wots", "wine", "vine", "wins", "wets", "gets", "wits", "bits", "bets", "pets", "wing", "bins", "gins", "beer", "bees", "beet", "beep", "weep", "deep"};
+        ArrayList<String> output = new ArrayList<String>();
+        for (String d : dummy) {
+            output.add(d);
+        }
+        return output;
+    };
 
     public ConfigurationScreen.GetJSONCallback gjsonc = new ConfigurationScreen.GetJSONCallback() {
         @Override
@@ -97,7 +107,7 @@ public class ConfigurationScreen extends AppCompatActivity {
         String ret_url = null;
         try {
             // Example URL: https://pixabay.com/api/?key=34155944-44bc9eeb96f0cc3d13b404eaa&q=yellow+flowers&image_type=photo
-            URL url = new URL("https://pixabay.com/api/?key=34155944-44bc9eeb96f0cc3d13b404eaa&q=" + word_source + "&image_type=photo");
+            URL url = new URL("https://pixabay.com/api/?key=34155944-44bc9eeb96f0cc3d13b404eaa&q=" + word_source + "&image_type=photo&editors_choice=true");
 
             HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
             con.setRequestMethod("GET");
@@ -109,11 +119,11 @@ public class ConfigurationScreen extends AppCompatActivity {
             while ((curr = in.readLine()) != null) {
                 data.append(curr);
             }
-            Log.d("ConfigurationScreen",data.toString());
+            Random r_idx = new Random();
             JSONObject jsonCatFact = new JSONObject(data.toString());
             JSONArray jsonArray = jsonCatFact.getJSONArray("hits");
-            JSONObject help = (JSONObject)jsonArray.get(0);
-            ret_url = (String)help.get("largeImageURL");
+            JSONObject help = (JSONObject)jsonArray.get(r_idx.nextInt(4));
+            ret_url = (String)help.get("userImageURL");
 
         }
         catch (MalformedURLException mue) {
@@ -226,8 +236,9 @@ public class ConfigurationScreen extends AppCompatActivity {
         if (word1.length() != word2.length()) {
             return null;
         }
-        ArrayList<String> output_list = new ArrayList<>(); output_list.add(word1);
-        result = find_matches(list, word1, word2, output_list, 0, word1.length());
+        ArrayList<String> output_of = new ArrayList<String>();
+        output_of.add(word1);
+        result = find_matches(list, word1, word2, output_of, 0, word1.length());
         return result;
     }
 
@@ -235,21 +246,21 @@ public class ConfigurationScreen extends AppCompatActivity {
         if (try_num == try_max) {
             return null;
         }
+        Log.d("ConfigurationScreen", curr);
         if (curr.equals(dest)) {
-            output_list.add(dest);
+            Log.d("ConfigurationScreen",output_list.toString());
             return output_list;
         }
         for (String entry : list) {
             if (entry.length() != 0) {
                 int correct = 0;
-                for (int i = 0; i < curr.length() - 1; i++) {
-                    correct += (entry.charAt(i) == curr.charAt(i) ? 1 : 0);
+                for (int i = 0; i < entry.length(); i++) {
+                    correct += (entry.charAt(i) == curr.charAt(i)) ? 1 : 0;
                 }
-                if (correct == curr.length() - 1) {
-                    ArrayList<String> temp = (ArrayList<String>) list.clone();
-                    temp.remove(curr);
+                Log.d("ConfigurationScreen",entry + " + " + curr + ": " + String.valueOf(correct));
+                if (correct == entry.length() - 1) {
                     output_list.add(entry);
-                    return find_matches(temp, entry, dest, output_list, try_num + 1, try_max);
+                    return find_matches(list, entry, dest, output_list, try_num+1, entry.length());
                 }
             }
         }
@@ -302,10 +313,11 @@ public class ConfigurationScreen extends AppCompatActivity {
                 String from = start_word.getText().toString();
                 String to = end_word.getText().toString();
                 if (from.length() > 0 && to.length() > 0) {
-                    ArrayList<String> word_list = scanFile(fileName, from, to);
+                    ArrayList<String> word_list = populate();
+                    //ArrayList<String> word_list = scanFile(fileName, from, to);
                     ArrayList<String> matches = search_for_match(word_list, from, to, from.length());
                     if (matches != null) {
-                        Toast.makeText(getApplicationContext(), matches.size(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), matches.toString(), Toast.LENGTH_SHORT).show();
                     }
                     else {
                         Toast.makeText(getApplicationContext(), "Could not find matches!", Toast.LENGTH_SHORT).show();
